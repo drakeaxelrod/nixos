3cf76906aae0b826cd207f28efee7ec1be662575
Drake Axelrod <drakeaxelrod@gmail.com>
2025-12-31 16:42:15 +0100

    update

--- users/draxel/home/desktop/gnome.nix
+++ users/draxel/home/desktop/gnome.nix
@@ -3,6 +3,15 @@
 
 {
   dconf.settings = {
+    # Wallpaper
+    "org/gnome/desktop/background" = {
+      picture-uri = "file://${config.home.homeDirectory}/.config/nixos/assets/wallpapers/nix-wallpaper-binary-red_8k.png";
+      picture-uri-dark = "file://${config.home.homeDirectory}/.config/nixos/assets/wallpapers/nix-wallpaper-binary-red_8k.png";
+      picture-options = "zoom";
+      primary-color = "#000000";
+      secondary-color = "#000000";
+    };
+
     # Interface
     "org/gnome/desktop/interface" = {
       color-scheme = "prefer-dark";
@@ -119,6 +128,56 @@
       edge-tiling = true;
       workspaces-only-on-primary = true;
     };
+
+    # GNOME Terminal
+    "org/gnome/terminal/legacy" = {
+      theme-variant = "dark";
+      default-show-menubar = false;
+    };
+
+    "org/gnome/terminal/legacy/profiles:" = {
+      default = "b1dcc9dd-5262-4d8d-a863-c897e6d979b9";
+      list = [ "b1dcc9dd-5262-4d8d-a863-c897e6d979b9" ];
+    };
+
+    "org/gnome/terminal/legacy/profiles:/:b1dcc9dd-5262-4d8d-a863-c897e6d979b9" = {
+      visible-name = "One Dark Pro";
+      use-system-font = false;
+      font = "Lilex Nerd Font 10";
+      use-theme-colors = false;
+      # One Dark Pro colors
+      foreground-color = "#abb2bf";
+      background-color = "#282c34";
+      bold-color = "#abb2bf";
+      bold-color-same-as-fg = true;
+      # Palette: Black, Red, Green, Yellow, Blue, Purple, Cyan, White (normal + bright)
+      palette = [
+        "#282c34"  # black
+        "#e06c75"  # red
+        "#98c379"  # green
+        "#e5c07b"  # yellow
+        "#61afef"  # blue
+        "#c678dd"  # purple
+        "#56b6c2"  # cyan
+        "#abb2bf"  # white
+        "#5c6370"  # bright black
+        "#e06c75"  # bright red
+        "#98c379"  # bright green
+        "#e5c07b"  # bright yellow
+        "#61afef"  # bright blue
+        "#c678dd"  # bright purple
+        "#56b6c2"  # bright cyan
+        "#ffffff"  # bright white
+      ];
+      cursor-colors-set = true;
+      cursor-foreground-color = "#282c34";
+      cursor-background-color = "#abb2bf";
+      highlight-colors-set = true;
+      highlight-foreground-color = "#abb2bf";
+      highlight-background-color = "#3e4451";
+      audible-bell = false;
+      scrollback-unlimited = true;
+    };
   };
 
   home.packages = with pkgs; [
@@ -126,5 +185,6 @@
     dconf-editor
     gnome-extension-manager
     gnome-disk-utility
+    gnome-terminal
   ];
 }
