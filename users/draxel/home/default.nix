d128003f3e2de9b483df47bf9794bf706a5df1ff
Drake Axelrod <drakeaxelrod@gmail.com>
2026-07-09 23:43:46 +0200

    disable nuvio import until Linux release URL is confirmed

--- users/draxel/home/default.nix
+++ users/draxel/home/default.nix
@@ -61,7 +61,7 @@ in
 
     # Applications
     modules.home.apps.moonlight
-    modules.home.apps.nuvio
+    # modules.home.apps.nuvio  # Disabled: no stable Linux release URL yet
     modules.home.apps.steam
     # modules.home.apps.stremio  # Disabled: requires orphaned qtwebengine-5.15.19 (see nixpkgs#437992)
                                   # Use web version instead: https://web.stremio.com
